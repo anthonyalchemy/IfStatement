84a7153479f29dc20b87e6c2418268d63f39c2fb
anthonyalchemy <anthony@alchemytechs.com>
2019-07-30 16:43:02 +0100

    Add project files.

---
+++ IfStatement/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace IfStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //if statement has two parts a condition then an action if true 
+            bool isMale = true;  //boolean variable 
+            bool isTall = true;
+
+            if (isMale && isTall)//putting a condition in the para //will only print if condition is true //&& = and || Or operator 
+            {
+                Console.WriteLine("You are a tall male");
+
+            }
+            else if (isMale && !isTall)//! operator NOT TALL 
+            {
+                Console.WriteLine("You are a short male");
+            }
+            else if (!isMale && isTall)
+            {
+                Console.WriteLine("You are not a male but you are tall");
+            }
+            else
+            {
+                Console.WriteLine("You are not male and not tall");//if both false it will print the statement 
+            }
+
+            //test the above method by changing around true and false
+
+
+
+            Console.ReadLine();
+           
+        }
+    }
+}
